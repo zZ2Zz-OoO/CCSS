1b848e5748da3a28b071b7776639ce41284b5cd3
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-22 15:33:06 +0800

    创建springboot项目

---
+++ src/main/java/com/zz/CcssApplication.java
@@ -0,0 +1,13 @@
+package com.zz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcssApplication.class, args);
+    }
+
+}

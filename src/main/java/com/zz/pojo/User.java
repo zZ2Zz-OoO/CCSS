f1d4c2f0e4d433136c3940ff039dafe95ddf6534
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-25 00:15:34 +0800

    新增序列化接口 标注表主键

--- src/main/java/com/zz/pojo/User.java
+++ src/main/java/com/zz/pojo/User.java
@@ -1,12 +1,17 @@
 package com.zz.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigInteger;
+
 @Data
-public class User {
+public class User implements Serializable {
+     @TableId(value = "user_id", type = IdType.ASSIGN_ID)
      private BigInteger userId;
-     private String userName ;
+     private String userName;
      private String password;
      private String phone;
      private String realName;

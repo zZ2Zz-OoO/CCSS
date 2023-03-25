1cd834ad2f27a29e4bbf8474d642d5700c538096
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-26 03:01:14 +0800

    添加mapper注解

--- src/main/java/com/zz/mapper/HouseKeeperMapper.java
+++ src/main/java/com/zz/mapper/HouseKeeperMapper.java
@@ -2,6 +2,8 @@ package com.zz.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zz.pojo.Housekeeper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface HouseKeeperMapper extends BaseMapper<Housekeeper> {
 }

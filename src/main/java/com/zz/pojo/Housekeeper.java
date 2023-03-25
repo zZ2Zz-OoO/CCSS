1ef73a7c73465a0bd49d0571902eeb69fa824c09
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-25 22:59:22 +0800

    修改命名方式 增加序列化 注解标明主键

--- src/main/java/com/zz/pojo/Housekeeper.java
+++ src/main/java/com/zz/pojo/Housekeeper.java
@@ -1,15 +1,19 @@
 package com.zz.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Date;
 
 
 @Data
-public class Housekeeper {
+public class Housekeeper implements Serializable {
+    @TableId(value = "house_keeper_id", type = IdType.ASSIGN_ID)
     private BigInteger houseKeepingId;
-    private String name;
+    private String houseKeeperName;
     private String password;
     private String companyName;
     private String address;

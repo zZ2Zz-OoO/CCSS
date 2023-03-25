846a85270212e2df46c423591e69d5be1d61b3f1
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-25 23:13:10 +0800

    添加Service注解

--- src/main/java/com/zz/service/Impl/HouseKeeperServiceImpl.java
+++ src/main/java/com/zz/service/Impl/HouseKeeperServiceImpl.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zz.mapper.HouseKeeperMapper;
 import com.zz.pojo.Housekeeper;
 import com.zz.service.HouseKeeperService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HouseKeeperServiceImpl extends ServiceImpl<HouseKeeperMapper, Housekeeper> implements HouseKeeperService {
 }

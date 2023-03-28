cfbce2ee8c9506dbc3f78d910413960be3999b5c
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-28 11:44:21 +0800

    新增服务评价类

---
+++ src/main/java/com/zz/pojo/ServiceComment.java
@@ -0,0 +1,16 @@
+package com.zz.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+@Data
+public class ServiceComment implements Serializable {
+    private BigInteger comment_id;
+    private BigInteger user_id;
+    private String context;
+    private String date;
+    private Integer comment_type;
+    private BigInteger service_order_id;
+}

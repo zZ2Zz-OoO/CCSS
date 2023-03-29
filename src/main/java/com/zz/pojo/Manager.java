8044a05918e56d2a43e2c7fd6f15ecc3ac539d4b
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-29 22:31:13 +0800

    添加tableId注解

--- src/main/java/com/zz/pojo/Manager.java
+++ src/main/java/com/zz/pojo/Manager.java
@@ -1,11 +1,15 @@
 package com.zz.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 
 @Data
-public class Manager {
+public class Manager implements Serializable {
+    @TableId(value = "manager_id", type = IdType.ASSIGN_ID)
     private BigInteger managerId;
     private String ManagerName;
     private String password;

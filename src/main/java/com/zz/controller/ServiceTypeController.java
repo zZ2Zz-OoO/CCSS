5e719dd03d997be7e3e372597a432fee56630278
zZ2Zz-OoO <56970771+zZ2Zz-OoO@users.noreply.github.com>
2023-03-30 16:44:00 +0800

    新增服务种类增删查改

---
+++ src/main/java/com/zz/controller/ServiceTypeController.java
@@ -0,0 +1,66 @@
+package com.zz.controller;
+
+import com.zz.pojo.R;
+import com.zz.pojo.ServiceType;
+import com.zz.service.ServiceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Types")
+public class ServiceTypeController {
+
+    @Autowired
+    private ServiceTypeService serviceTypeService;
+
+    @PostMapping
+    public R<String> addServiceType(@RequestBody ServiceType ServiceType) {
+        boolean save = serviceTypeService.save(ServiceType);
+        if (save) {
+            return R.success("添加种类成功");
+        }
+        else {
+            return R.error("添加种类失败");
+        }
+    }
+
+    @DeleteMapping("/{ServiceType_id}")
+    public R<String> deleteOne(@PathVariable BigInteger ServiceType_id) {
+        boolean b = serviceTypeService.removeById(ServiceType_id);
+        if (b) {
+            return R.success("删除成功");
+        }
+        else {
+            return R.error("删除失败");
+        }
+    }
+
+    @GetMapping("/{ServiceType_id}")
+    public R<ServiceType> getById(@PathVariable BigInteger ServiceType_id) {
+        ServiceType ServiceType = serviceTypeService.getById(ServiceType_id);
+        if (ServiceType != null) {
+            return R.success(ServiceType);
+        }
+        return R.error("无此种类");
+    }
+
+    @GetMapping
+    public R<List<ServiceType>> getAll() {
+        List<ServiceType> serviceTypes = serviceTypeService.list();
+        return R.success(serviceTypes);
+    }
+
+    @PutMapping
+    public R<String> update(@RequestBody ServiceType ServiceType) {
+        boolean flag = serviceTypeService.updateById(ServiceType);
+        if (flag) {
+            return R.success("修改成功");
+        }
+        else {
+            return R.error("修改失败");
+        }
+    }
+}
